2bd55b06538516ea7d02ccb3ed428a188e8b86ab
shishishi76 <0706shogo@gmail.com>
2023-05-21 23:22:34 +0900

    a

---
+++ practice_hol/src/components/Recipe.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Recipe({ name, ingredients, steps }) {
+  return (
+    <section id="baked-salmon">
+      <h1>shogo</h1>
+      <h1>{name}</h1>
+      <ul className="ingredients">
+        {ingredients.map((ingredient, i) => (
+          <li key={i}>{ingredient.name}</li>
+        ))}
+      </ul>
+      <section className="instructions">
+        <h2>Cooking Instructions</h2>
+        {steps.map((step, i) => (
+          <p key={i}>{step}</p>
+        ))}
+      </section>
+    </section>
+  );
+}
+
+export default Recipe;

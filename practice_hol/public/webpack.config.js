6e7e6e995d3275168e04a2cbe38736a61360a925
shishishi76 <0706shogo@gmail.com>
2023-05-21 23:58:52 +0900

    ビルド環境の構築

---
+++ practice_hol/public/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+  path: path.join(__dirname, "dist", "assets"),
+  filename: "bundle.js"
+  },
+  module: {
+  rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+  };

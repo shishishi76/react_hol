c7155bf83004ff945b97625825569de7e49625dc
shishishi76 <0706shogo@gmail.com>
2023-05-21 23:53:55 +0900

    レシピアプリ

--- practice_hol/src/components/Recipe.js
+++ practice_hol/src/components/Recipe.js
@@ -1,23 +1,13 @@
 import React from "react";
+import IngredientsList from "./IngredientsList";
+import Instructions from "./Instructions";
 
-function Recipe({ name, ingredients, steps }) {
+export default function Recipe({ name, ingredients, steps }) {
   return (
-    <section id="baked-salmon">
-      <h1>shogo</h1>
+    <section id={name.toLowerCase().replace(/ /g, "-")}>
       <h1>{name}</h1>
-      <ul className="ingredients">
-        {ingredients.map((ingredient, i) => (
-          <li key={i}>{ingredient.name}</li>
-        ))}
-      </ul>
-      <section className="instructions">
-        <h2>Cooking Instructions</h2>
-        {steps.map((step, i) => (
-          <p key={i}>{step}</p>
-        ))}
-      </section>
+      <IngredientsList list={ingredients} />
+      <Instructions title="Cooking Instructions" steps={steps} />
     </section>
   );
 }
-
-export default Recipe;
--- practice_hol/src/index.js
+++ practice_hol/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { render } from "react-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import data from "..//src/data/recipes.json"
+import Menu from './components/Menu';
 import Recipe from './components/Recipe';
 
+render(<Menu recipes={data} />, document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
